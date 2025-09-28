48afec5d391b1d76e2a67baa0fd79bce5a35205f
arthurd3 <arthur_camposl@yahoo.com>
2025-09-28 18:31:08 -0300

    feat: configure websocket

---
+++ app/src/main/java/com/realchat/app/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.realchat.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // set message broker
+        registry.enableSimpleBroker("/topic");
+
+        // expect message with /app/sendmessage
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
